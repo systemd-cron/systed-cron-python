d363a333f6622a39f1723f5a2a61a75e3f4918af
наб <nabijaczleweli@nabijaczleweli.xyz>
2023-08-11 22:33:05 +0200

    Port the tests!

--- src/bin/mail_on_failure.sh
+++ src/bin/mail_on_failure.sh
@@ -30,8 +30,8 @@ systemctl show --property=User --property=Environment "$unit" | {
 
 	for kv in $job_env; do
 		case "$kv" in
-			'MAILTO='*  )	mailto="${kv%'MAILTO='}"     ;;
-			'MAILFROM='*)	mailfrom="${kv%'MAILFROM='}" ;;
+			'MAILTO='*  )	mailto="${kv#'MAILTO='}"     ;;
+			'MAILFROM='*)	mailfrom="${kv#'MAILFROM='}" ;;
 		esac
 	done
 
